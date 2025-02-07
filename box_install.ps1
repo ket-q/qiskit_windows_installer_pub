bc268a36bf586760faaa01733c134cd2ea972716
Bernd Burgstaller <9303437+berndburgstaller@users.noreply.github.com>
2025-02-07 21:23:30 +0900

    require Python 3.12

--- box_install.ps1
+++ box_install.ps1
@@ -2,7 +2,7 @@ Write-Host "Elevating to Administrator rights..."
 
 Disable-UAC
 
-choco install -y --version=3.12 # requirement of 'ray' of ibm-serverless
+choco install -y python --version=3.12 # requirement of 'ray' of ibm-serverless
 
 choco install -y pyenv-win
     

4a8f7374750d479d4d45d78b974037d8f6ded87c
Bernd Burgstaller <9303437+berndburgstaller@users.noreply.github.com>
2025-02-16 19:40:08 +0900

    fixed call script &

--- q_install.ps1
+++ q_install.ps1
@@ -2,49 +2,12 @@
 $ErrorActionPreference = 'Stop'
 $PSNativeCommandUseErrorActionPreference = $true
 
-$python_version = "3.12.9" # 3.13 not working because ray submodule wants Python 3.12
+$python_version = "3.12.9" # 3.13 not working because ray requires Python 3.12
 $qiskit_version = "1.3.2"
 # Name of venv in .virtualenvs
-$qwi_vstr = "qiskit_" + $qiskit_version.Replace(".", "_") + "_p" + $python_version.Replace(".", "_")
+$qwi_vstr = "qiskit_" + $qiskit_version.Replace(".", "_")
 # Name of the requirements.txt file to download from GitHub:
 $requirements_file = "latest_requirements.txt"
-
-# # Elevating to Administrator rights..."
-
-# # Get the ID and security principal of the current user account
-# $myWindowsID = [System.Security.Principal.WindowsIdentity]::GetCurrent();
-# $myWindowsPrincipal = New-Object System.Security.Principal.WindowsPrincipal($myWindowsID);
-
-# # Get the security principal for the administrator role
-# $adminRole = [System.Security.Principal.WindowsBuiltInRole]::Administrator;
-
-# # Check to see if we are currently running as an administrator
-# if ($myWindowsPrincipal.IsInRole($adminRole))
-# {
-#     # We are running as an administrator, so change the title and background colour to indicate this
-#     $Host.UI.RawUI.WindowTitle = $myInvocation.MyCommand.Definition + "(Elevated)";
-#     #$Host.UI.RawUI.BackgroundColor = "DarkBlue";
-#     #Clear-Host;
-#     Write-Host "Script running as administrator..."
-# } else {
-#     # We are not running as an administrator, so relaunch as administrator
-#     Write-Host "Script not running as administrator..."
-
-#     # Create a new process object that starts PowerShell
-#     $newProcess = New-Object System.Diagnostics.ProcessStartInfo "PowerShell";
-
-#     # Specify the current script path and name as a parameter with added scope and support for scripts with spaces in it's path
-#     $newProcess.Arguments = "& '" + $script:MyInvocation.MyCommand.Path + " -NoExit'"
-
-#     # Indicate that the process should be elevated
-#     $newProcess.Verb = "runas";
-
-#     # Start the new process
-#     [System.Diagnostics.Process]::Start($newProcess);
-
-#     # Exit from the current, unelevated, process
-#     Exit;
-# }
 
 function Write-Header {
     param(
@@ -223,36 +186,26 @@ function Download-File {
     Write-Host "Download DONE"
 }
 
-function Install-VSC {
-    param (
-        [Parameter(Mandatory = $true, Position = 1, HelpMessage = "local or global install")]
-        [ValidateSet('local','global')]
-        [string[]]$Scope = 'global'
-    )
-    $Destination = "$env:TEMP\vscode_installer.exe"
-    $VSCodeUrl = "https://code.visualstudio.com/sha/download?build=stable&os=win32-x64"
-
-    # User vs. system-wide installation
-    if ($Scope  -eq 'local') {
-        $VSCodeUrl = $VSCodeUrl + '-user'
-    }
-
-    $UnattendedArgs = '/verysilent /mergetasks=!runcode'
+function Install-VSCode {
+    $VSCode_installer = "vscode_installer.exe"
+    $VSCode_installer_path = Join-Path ${env:TEMP} -ChildPath $VSCode_installer
+    # Download the local installer by appending '-user' to the download URL:
+    $VSCode_URL = "https://code.visualstudio.com/sha/download?build=stable&os=win32-x64-user"
 
     # Download VSCode
     Write-Host "Downloading VSCode"
-    $ProgressPreference = 'SilentlyContinue' # omit progress update to favour fast download time
-    Invoke-WebRequest -Uri $VSCodeUrl -OutFile $Destination
+    Download-File $VSCode_URL $VSCode_installer_path
 
     # Install VSCode
     Write-Host "Installing VSCode"
-    Start-Process -FilePath $Destination -ArgumentList $UnattendedArgs -Wait -Passthru
+    $unattended_args = '/VERYSILENT /MERGETASKS=!runcode'
+    Start-Process -FilePath $VSCode_installer_path -ArgumentList $unattended_args -Wait -Passthru
 
     # Cleanup
-    Remove-Item $Destination
+    Remove-Item $VSCode_installer_path
 }
 
-function Install-VSC-Extension {
+function Install-VSCode-Extension {
     param (
         [Parameter(
             Mandatory = $true,
@@ -277,15 +230,10 @@ function Install-pyenv-win {
 }
 
 
-
 #
 # Main
 #
-# [void] (Get-ChildItem -Path "C:\Users" -ErrorAction SilentlyContinue -ErrorVariable err_1)
-# [void] (Get-ChildItem -Path "C:\foo" -ErrorAction SilentlyContinue -ErrorVariable err_2)
-# Log-Err 'fatal' 'The paths I tried' $err_1 $err_2
-# Write-Host 'got past'
-# Exit 0
+
 Set-ExecutionPolicy Bypass -Scope Process -Force
 
 #
@@ -294,16 +242,16 @@ Set-ExecutionPolicy Bypass -Scope Process -Force
 Write-Header "Step 1: Install VSCode"
 if (!(Get-Command code -ErrorAction SilentlyContinue) ) {
     Write-Host("VSCode not not installed, running installer")
-    Install-VSC local
+    Install-VSCode
     Refresh-PATH
 } else {
-    Write-Host("VSCode installed")
+    Write-Host("VSCode already installed")
 }
 Write-Host "Installing VSCode Python extension"
-Install-VSC-Extension "ms-python.python"
+Install-VSCode-Extension "ms-python.python"
 
 Write-Host "Installing VSCode Jupyter extension"
-Install-VSC-Extension "ms-toolsai.jupyter"
+Install-VSCode-Extension "ms-toolsai.jupyter"
 
 #
 # pyenv-win
@@ -332,8 +280,6 @@ if ( !(Get-Command pyenv -ErrorAction SilentlyContinue) ) {
 # Python
 #
 Write-Header "Step 3: Install Python $python_version"
-# Write-Host "$env:LOCALAPPDATA"
-# Write-Host "You are in ${env:USERPROFILE}"
 $QINST_ROOT = "${env:LOCALAPPDATA}\qiskit_windows_installer"
 if (!(Test-Path $QINST_ROOT)){
     New-Item -Path "$QINST_ROOT" -ItemType Directory
@@ -357,9 +303,9 @@ Write-Header "Step 4: set up enclave folder"
 try {
     $QINST_ENCLAVE = Join-Path $QINST_ROOT -ChildPath $qwi_vstr
     if (!(Test-Path $QINST_ENCLAVE)){
-         New-Item -Path "$QINST_ENCLAVE" -ItemType Directory
+         $err = New-Item -Path "$QINST_ENCLAVE" -ItemType Directory
     }
-    Set-Location -Path "$QINST_ENCLAVE"
+    $err = Set-Location -Path "$QINST_ENCLAVE"
 }
 catch {
     $err_msg = (
@@ -415,23 +361,28 @@ if ( !($dot_venvs_dir_obj.PSIsContainer) ) {
 }
 
 # Test whether a venv of name $qwi_vstr already exists and delete it.
-# Note that VSCode etc. should not use the venv in that moment, but we don't
+# Note 1: VSCode etc. should not use the venv in that moment, but we don't
 # actually check this.
+# FIXME: If a Jupyter notebook is open, then the rm command on the venv
+#        will fail.
 
 $MY_VENV_DIR = Join-Path ${DOT_VENVS_DIR} -ChildPath $qwi_vstr
 
 try {
     if (Test-Path $MY_VENV_DIR) {
         # A venv of that name seems to exist already -> remove
-        rm -R $MY_VENV_DIR
+        #rm -R $MY_VENV_DIR
+        # Remove-Item -Force -Recurse -Path $MY_VENV_DIR
+        Get-ChildItem $MY_VENV_DIR -Recurse | Remove-Item -Force -Recurse
     }
 }
 catch {
-    $err_msg = (
+    $err_1 = $($_.Exception.Message)
+    $err_0 = (
         "Unable to remove existing venv.",
         "Path/venv: $MY_VENV_DIR"
         ) -join "`r`n"
-    Fatal-Error $err_msg 1
+    Log-Err 'fatal' $err_0 $err_1
 }
 
 
@@ -471,100 +422,75 @@ Download-File `
 
 # Create venv
 Write-Header "Step 6: Set up venv $MY_VENV_DIR"
-Invoke-Command { pyenv exec python -m venv $MY_VENV_DIR } # `
-#     -ErrorAction SilentlyContinue `
-#     -ErrorVariable err_create_venv
-# Activate venv
-& "${MY_VENV_DIR}\Scripts\activate.ps1"
-#    -ErrorAction SilentlyContinue `
-#    -ErrorVariable err_venv_activate
-#& pyenv exec pip --version
+try {
+    # create venv
+    & pyenv exec python -m venv $MY_VENV_DIR
+    # activate venv
+    & "${MY_VENV_DIR}\Scripts\activate.ps1"
+}
+catch {
+    Log-Err 'fatal' 'Setting up venv' $($_.Exception.Message) 1
+}
 
 #
+# venv activated
 # PATH now includes our venv
 #
 
 # Update pip of venv
-& python -m pip install --upgrade pip
-#    -ErrorAction SilentlyContinue `
-#    -ErrorVariable err_pip_upgr
-# Install Qiskit in venv
-& pip install -r $requirements_file
-#    -ErrorAction SilentlyContinue `
-#    -ErrorVariable err_pip_install_qiskit
-
-# Install ipykernel module
-& pip install ipykernel
-#    -ErrorAction SilentlyContinue `
-#    -ErrorVariable err_pip_install_ipykernel
-# Install Jupyter server in venv
-#$mod = "-m ipykernel install"
-#$mod_args = "--user --name=$qwi_vstr --display-name `"$qwi_vstr`""
-& python -m ipykernel install --user --name=$qwi_vstr --display-name "$qwi_vstr"
-# Invoke-Command { pyenv exec python $mod $mod_args } `
-#     -ErrorAction SilentlyContinue `
-#     -ErrorVariable err_venv_install_Jupyter
-
-# Log-Err 'fatal' 'venv setup in .virtualenvs failed' `
-#     $err_create_venv $err_venv_activate $err_pip_upgr `
-#     $err_pip_install_qiskit $err_pip_install_ipykernel `
-#     $err_venv_install_Jupyter
-
-
-
-Exit 0
-
-# Delete a pre-existing bootstrap venv
+Write-Header "Step 7: update pip of venv $MY_VENV_DIR"
 try {
-    if (Test-Path $boot_venv_name) {
-        # The bootstrap venv seems to exist already -> remove
-        rm -R .\$boot_venv_name
-    }
+    & python -m pip install --upgrade pip
 }
 catch {
-    $err_msg = (
-        "Unable to remove existing bootstrap venv in enclave.",
-        "Path: $QINST_ENCLAVE",
-        "Name of venv: '$boot_venv_name'"
-        ) -join "`r`n"
-    Fatal-Error $err_msg 1
+    Log-Err 'fatal' 'Update pip of venv $MY_VENV_DIR' $($_.Exception.Message) 1
 }
 
-# Create bootstrap venv
-Invoke-Command { pyenv exec python -m venv $boot_venv_name } `
-     -ErrorAction SilentlyContinue `
-     -ErrorVariable err_boot_venv
-# Activate bootstrap venv
-Invoke-Command { ".\${boot_venv_name}\Scripts\activate" } `
-    -ErrorAction SilentlyContinue `
-    -ErrorVariable err_boot_venv_activate
-# Update pip of bootstrap venv
-Invoke-Command { pyenv exec python -m pip install --upgrade pip } `
-    -ErrorAction SilentlyContinue `
-    -ErrorVariable err_boot_pip_upgr
-# Install pipenv in venv
-Invoke-Command { pyenv exec pip install pipenv } `
-    -ErrorAction SilentlyContinue `
-    -ErrorVariable err_boot_pipenv
-Log-Err 'fatal' 'bootstrap venv setup in enclave' `
-    $err_boot_venv $err_boot_venv_activate $err_boot_pip_upgr $err_boot_pipenv
+# Install ipykernel module in venv
+Write-Header "Step 8: install ipykernel module in venv $MY_VENV_DIR"
+try {
+    & pip install ipykernel
+}
+catch {
+    $err = $($_.Exception.Message)
+    $err_args = 'fatal',
+        'ipykernel module installation in venv $MY_VENV_DIR',
+        $err,
+        1
+    Log-Err @err_args
+}
 
-# Create 'official' venv
+# Install Qiskit in venv
+Write-Header "Step 9: install Qiskit in venv $MY_VENV_DIR"
+try {   
+    & pip install -r $requirements_file
+}
+catch {
+    $err = $($_.Exception.Message)
+    $err_args = 'fatal',
+        'Qiskit installation in venv $MY_VENV_DIR',
+        $err,
+        1
+    Log-Err @err_args
+}
 
-# Since 2024 part of Win 10/11, make sure to use the exe prefix to avoid quirk:
-# curl.exe --version
+# Install Jupyter server in venv
+Write-Header "Step 10: install ipykernel kernel in venv $MY_VENV_DIR"
+try {
+    $args = "-m", "ipykernel", "install",
+        "--user",
+        "--name=$qwi_vstr",
+        "--display-name", "`"$qwi_vstr`""
+    & python $args
+}
+catch {
+    $err = $($_.Exception.Message)
+    $err_args = 'fatal',
+        'Qiskit installation in venv $MY_VENV_DIR',
+        $err,
+        1
+    Log-Err @err_args
+}
 
-# w/o pipenv (rationale: pipenv is extremely slow in installing packages):
-# pyenv exec python -m venv C:\Users\bburg\.virtualenvs\my_test
-# cd C:\Users\bburg\.virtualenvs\  # not needed
-# .\my_test\Scripts\activate
-# (my_test) PS C:\Users\bburg> python -m pip install --upgrade pip
-# pip install ipykernel
-# pip install -r requirements.txt
-# python -m ipykernel install --user --name=my_qiskit --display-name "my_qiskit"  # restart VSCode for Jupyter kernel to become visible
-
-
-# old:
-#pyenv exec pipenv install -r requirements.txt
-#pyenv exec pipenv install
-#pyenv exec pipenv install ipykernel
+# Done
+Exit 0
